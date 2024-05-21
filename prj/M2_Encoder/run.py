acf0652a362801ff657a9729927875f26b7e987a
嵩竹 <zhanghanxiao.zhx@antgroup.com>
2024-05-21 17:42:13 +0800

    add 1B and 10B M2_Encoder models

--- prj/M2_Encoder/run.py
+++ prj/M2_Encoder/run.py
@@ -23,3 +23,12 @@ data = {
 
 res = encoder.local_inference(data)
 print(res)
+
+# result of Encoder_0.4B.json
+# [[3.3752596e-07 7.2387557e-08 2.4727814e-07 9.9999928e-01]]
+
+# result of Encoder_1B.json
+# [[1.5551204e-06 4.3255361e-07 7.5551941e-07 9.9999726e-01]]
+
+# result of Encoder_10B.json
+# [[6.2423642e-05 2.7531558e-05 6.2977582e-05 9.9984785e-01]]
